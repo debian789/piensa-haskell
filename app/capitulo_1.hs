b0f45c8a48125b266277ef57b718240c9168e6e9
Miguel Suescun <debian789@gmail.com>
2022-04-19 07:39:41 -0500

    Actualizacion de comentarios capitulo 1

--- app/capitulo_1.hs
+++ app/capitulo_1.hs
@@ -1,9 +1,11 @@
+-- 1.1. Media de 3 números
 media3 :: Fractional a => a -> a -> a -> a
 media3 a b c = do 
     (a + b + c) / 3 
 -- R: media3 2 3 4                       3.0
 
 
+-- 1.2. Suma de euros de una colección de monedas
 sumaMonedas :: Num a => a -> a -> a -> a -> a -> a
 sumaMonedas a b c d e  = 
     aa + bb +cc +dd +ee
@@ -15,33 +17,47 @@ sumaMonedas a b c d e  =
         ee = e * 20
 -- R: sumaMonedas 1 2 3 4 5              160
 
+
+-- 1.3. Volumen de la esfera
 volumenEsfera2 :: Floating a => a -> a
 volumenEsfera2 a = (4/3) * pi * (a^3)
 -- R: volumenEsfera2 10                  4188.790204786391
 
+
+-- 1.4. Área de una corona circular
 areaDeCoronaCircular :: Floating a => a -> a -> a
 areaDeCoronaCircular r1 r2 = pi * (r2 ^ 2  - r1 ^ 2)
 -- R: areaDeCoronaCircular 10 100         31101.767270538952
 
+
+-- 1.5. Última cifra de un número
 ultimaCifra :: Show a => a -> Char
 ultimaCifra a = last (show(a))
 -- R:  ultimaCifra 789                    '9'
 
+
+-- 1.5. Última cifra de un número
 ultimaCifra' :: Integral a => a -> a
 ultimaCifra' a = rem a 10
 -- ultimaCifra' 789                        9
 
+
+-- 1.6. Máximo de 3 elementos
 maxTres :: Ord a => a -> a -> a -> a
 maxTres a b c 
     | a > b && a > c = a
     | b > c && b > a = b
     | c > b && c > a = c
 -- R:  maxTres 10 22 3                     22
-    
+
+
+-- 1.6. Máximo de 3 elementos
 maxTres' :: Ord a => a -> a -> a -> a
 maxTres' a b c = max a (max b c)
 -- maxTres' 10 22 3                        22
 
+
+-- 1.7. Disyunción excluyente
 xor1 :: Eq a => a -> a -> Bool
 xor1 a b 
     | a == b = False
@@ -49,38 +65,46 @@ xor1 a b
 -- R: xor1 3 3                             False
 
 
+-- 1.8. Rotación de listas
 rotar1 :: [a] -> [a]
 rotar1 [] = []
 rotar1 (x:xs) = rotar1 xs ++ [x]
 -- R: rotar1 [1,2,3,4]                     [4,3,2,1]
 
 
+-- 1.8. Rotación de listas
 rotar1' :: [a] -> [a]
 rotar1' a = reverse a
 -- R: rotar1' [1,2,3,4]                    [4,3,2,1]
 
+
+-- 1.8. Rotación de listas
 rotar1'' :: [a] -> [a]
 rotar1'' xs =  tail xs ++ [head xs]
 -- R: rotar1'' [1,2,3,4]                   [2,3,4,1]
 
 
+-- 1.8. Rotación de listas
 rota :: Int -> [a] -> [a]
 rota a xs =   drop a xs ++ take a xs 
 -- R: rota 2 [6,7,9,10]                    [9,10,6,7]
 
+
+-- 1.9. Rango de una lista
 rango :: (Foldable t, Ord a) => t a -> [a]
 rango xs = [minimum xs, maximum xs]
 -- R: rango  [12,8,7,10,11]                [7,12]
 
 
--- Reconocimiento de aplindromos 
-
+-- 1.10. Reconocimiento de palíndromos
 palindromo :: Eq a => [a] -> Bool
 palindromo a 
     | a == reverse a = True
     | otherwise      = False
 -- R: palindromo [3,2,3]                    True
 
+
+-- 1.10. Reconocimiento de palíndromos
 palindromo' :: Eq a => [a] -> Bool
 palindromo' a = a == reverse a
 -- R: palindromo' [3,2,3]                    True
@@ -91,24 +115,30 @@ interior :: [a] -> [a]
 interior xs = init(tail xs)
 -- R: interior [1,2,3,4,5]                   [2,3,4]
 
+
+-- 1.11. Elementos interiores de una lista
 interior' :: [a] -> [a]
 interior' xs =  tail(init xs)
 -- R: interior' [1,2,3,4,5]                  [2,3,4]
+
 
 -- 1.12 Finales de una lista
 finales :: Int -> [a] -> [a]
 finales n xs = drop ((length xs) - n) xs 
 -- R: finales 3 [1,2,3,4]                    [2,3,4]
 
+
 -- 1.13 Segmentos de una lista 
 segmentos :: Int -> Int -> [a] -> [a]
 segmentos a b xs = drop (a-1) (take b xs) 
 -- R: segmentos  2 4 [9,8,7,6,5]              [8,7,6]
 
+
 -- 1.14 Extremos de una lista 
 extremos :: Int -> [a] -> [a]
 extremos a xs = take a xs ++ drop (length xs - a) xs
 -- R: extremos   6 [1,2,3]                    [1,2,3,1,2,3]
+
 
 -- 1.15 Mediano 3 numeros
 mediano :: (RealFrac a, Integral b) => a -> a -> a -> b
@@ -121,6 +151,8 @@ tresIguales :: Eq a => a -> a -> a -> Bool
 tresIguales a b c = (a == b && b == c && c == a)
 -- R: tresIguales 6 6 6                          True 
 
+
+-- 1.16. Igualdad y diferencia de 3 elementos
 tresDiferentes :: Eq a => a -> a -> a -> Bool
 tresDiferentes a b c = (a /= b && b /= c && a /= c )
 -- R: tresDiferentes 3 4 5                       True 
@@ -131,12 +163,14 @@ cuatroIguales :: Eq a => a -> a -> a -> a -> Bool
 cuatroIguales a b c d = tresIguales a b c && tresIguales b c d 
 -- R: cuatroIguales 5 5 5 5                      True
 
+
+-- 1.17 Igual de 4 Elementos
 cuatroIguales' :: Eq a => a -> a -> p -> a -> Bool
 cuatroIguales' a b c d = a == b && tresIguales a b d
 -- R: cuatroIguales' 5 5 5 5                     True
 
 
--- Propiedad triangular 
+-- 1.18. Propiedad triangular
 triangular :: (Ord a, Num a) => a -> a -> a -> Bool
 triangular a b c = (a + b > c) && (a + c > b) && (c + b > a)
 -- R: triangular 3 3 3                           True
@@ -149,15 +183,19 @@ divisionSegura a b
     | otherwise = 9999.0
 -- R: divisionSegura  4 0                        9999.0
 
+
+-- 1.19. División segura
 divisionSegura' :: (Eq p, Fractional p) => p -> p -> p
 divisionSegura' _ 0 = 9999.0
 divisionSegura' a b = a / b 
 -- R: divisionSegura'  4 0                       9999.0
 
+
 -- 1.21 Modulo de un vector
 modulo :: Floating a => (a, a) -> a
 modulo (a, b) = sqrt(a^2 + b ^2 )
 -- R: modulo (3,4)                                5.0
+
 
 -- 1.22 Rectangulo de area maxima 
 mayorRectangulo :: (Ord b, Num b) => (b, b) -> (b, b) -> (b, b)
@@ -177,9 +215,11 @@ cuadrante (a,b)
 -- R: cuadrante (-6,3)                              2
 
 
+-- 1.23.2. Intercambio de coordenadas
 intercambia :: (b, a) -> (a, b)
 intercambia (a, b) = (b,a)
 -- R: intercambia (10,2)                           (2,10)
+
 
 -- 1.23.3 Punto simetrico 
 simetricoH :: (Ord a1, Num a1) => (a2, a1) -> (a2, a1)
@@ -189,30 +229,36 @@ simetricoH (a,b)
 -- R: simetricoH (2,3)                             (2,-3)
 
 
+-- 1.23.3 Punto simetrico 
 simetricoH' :: Num b => (a, b) -> (a, b)
 simetricoH' (a,b) = (a,-b)
 -- R: simetricoH' (2,3)                            (2,-3)
+
 
 -- 1.23.4 Distancia entre dos puntos
 distancia :: Floating a => (a, a) -> (a, a) -> a
 distancia (a1,b1) (a2,b2) = sqrt( (a2 - a1) ^ 2 + (b2 -b1) ^ 2 )
 -- R: distancia (2,2) (4,5)                         3.605551275463989
 
--- punto medio entre otros dos 
+
+-- 1.23.5. Punto medio entre otros dos
 puntoMdia :: (Fractional a, Fractional b) => (a, b) -> (a, b) -> (a, b)
 puntoMdia (a1,b1) (a2,b2) = ((a2 + a1)/2 , (b2 + b1)/2 )
 -- R:  puntoMdia  (2,2) (4,5)                       (3.0,3.5)
+
 
 -- 1.25 Intercalacion de pares
 intercala :: [a] -> [a] -> [a]
 intercala [x1,xs1] [x2, xs2] = [x1,xs2,x2,xs1]
 -- R: intercala [1,2] [3,4]                         [1,4,3,2]
 
--- Permutacion ciclica de una lista
+
+-- 1.26. Permutación cíclica de una lista
 ciclo :: [a] -> [a]
 ciclo [] = []
 ciclo xs = last xs : init xs
 -- R: ciclo [1,2,3,4]                               [4,1,2,3]
+
 
 -- 1.27 Mayor numero de 2 cifras con dos digitos dados 
 numeroMayor :: (Show a2, Show a1) => a2 -> a1 -> Int
@@ -227,11 +273,14 @@ numeroMayor a b =
         numer2 = read bb :: Int
 -- R: numeroMayor 1 24                               241
 
+
+-- 1.27. Mayor número de 2 cifras con dos dígitos dados
 numeroMayor' :: (Num a, Ord a) => a -> a -> a
 numeroMayor' x y  = a*10 + b
     where a = max x y
           b = min x y  
 -- R: numeroMayor' 1 24                              241
+
 
 -- 1.28 Numero de reaices de una ecuacion cuadratica 
 numeroDeRaices :: Int -> Int -> Int -> Int
@@ -244,7 +293,8 @@ numeroDeRaices a b c
         d = b ^ 2 - (4 * (a*c))
 -- R: numeroDeRaices 20 33 4                         2
 
--- raices de las ecucaciones cuadraticas 
+
+-- 1.29. Raíces de las ecuaciones cuadráticas
 raices :: Floating a => a -> a -> a -> [a]
 raices a b c 
     | otherwise  = [positivo , negativo]
@@ -258,16 +308,19 @@ raices a b c
 -- R: raices 11 42 33                                   [-1.1062035576065592,-2.711978260575259]
 
 
+-- 1.29. Raíces de las ecuaciones cuadráticas
 raices' :: Floating a => a -> a -> a -> [a]
 raices' a b c = [(-b+d)/t, (-b-d)/t]
     where d = sqrt (b^2 - 4*a*c)
           t = 2 * a
 -- R:  raices' 11 42 33                                 [-1.1062035576065592,-2.711978260575259]
 
+
 -- 1.30 Area de un triangulo mediante la formula de Heron
 area :: Fractional a => a -> a -> a -> a
 area a b c = (a+b+c)/2
 --  area 4 5 6                                           7.5
+
 
 -- 1.31 Numeros racionales como pares de enteros 
 formaReducida :: (Integral a, Integral b) => (a, b) -> (a, b)
